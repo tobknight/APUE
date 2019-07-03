3bc57fcc006e9aa0b9f974fde740e0c4aa7a831d
Amadeus Kurisu <Amadeus Kurisu@ether.com>
2019-07-03 03:22:16 -0400

    F15.5 Fork will process both parent and child process

---
+++ F15.5/F15.5.c
@@ -0,0 +1,25 @@
+#include "apue.h"
+
+int main(void){
+    int n;
+    int fd[2];
+    pid_t pid;
+    char line[MAXLINE];
+
+    if(pipe(fd) < 0)
+        err_sys("pipe error");
+    if((pid = fork()) < 0){
+        err_sys("fork error");
+    }else if( pid > 0 ){/*Parent*/
+        printf("Parent: %d \n", getpid());
+        close(fd[1]);
+        write(fd[1], "hello world\n", 12);
+    } else{
+        printf("Child: %d\n", getpid());
+        close(fd[1]);
+        n = read(fd[0], line, MAXLINE);
+        write(STDOUT_FILENO, line, n);
+    }
+
+    exit(0);
+}
